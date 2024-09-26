079fa769c4b8eac28f006640517704609d64c212
almightobasadre <alau.basadre.up@phinmaed.com>
2024-09-26 13:12:33 +0800

    Added game objects

--- app/main.py
+++ app/main.py
@@ -1,19 +1,44 @@
 import pygame
+# Only importing the function to use for performance
+from os.path import join
+from random import randint
 
-# General Setup
+# Initialize Pygame and set up the game window
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Creeps Shooter")
 running = True
 
+# # Plain Surface
+# surf = pygame.Surface((50, 50))
+# surf.fill('orange')
+x = 100
+
+# Import image using os.path.join() for cross-platform file path compatibility instead of hardcoding paths like "dir/dir/file"
+# Use convert() for better performance, _alpha is added for images that have transparent pixels
+player_surf = pygame.image.load(join('assets/art', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('assets/art', 'star.png')).convert_alpha()
+
+# Generate random (x, y) positions for stars and store them in star_positions using List Comprehension
+star_count = 22
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(star_count)] 
+
 while running:
-    # Event Loop
+    # Event loop that keeps updating frames to display content until exiting
     for event in pygame.event.get():
-        if event.type ==    pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
     
-    # Draw the Game
-    display_surface.fill(color=(42, 62, 82)) # Charcoal: 43, 61, 79 / Night: All 10-22
+    # Draw the game components
+    display_surface.fill(color=(43, 61, 79)) # Charcoal: 43, 61, 79 / Night: All 10-22
+    x += 0.1
+    display_surface.blit(player_surf, (x, 590))
+
+    # Draw stars using their pre-generated list index
+    for pos in star_positions:
+        display_surface.blit(star_surf, (pos))
+
     pygame.display.update()
 
 pygame.quit()
