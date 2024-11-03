7e097ff6b0a4743eaccdbc6ad9cbff7f39cb5379
almightobasadre <alau.basadre.up@phinmaed.com>
2024-11-03 13:14:19 +0800

    Changed screen color and title

--- app/main.py
+++ app/main.py
@@ -4,13 +4,18 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # only display once
+pygame.display.set_caption("Creeps Shooter")
 
 running = True
 
 while running:
-    for event in pygame.event.get(): # game loop
+    for event in pygame.event.get(): # event loop
         if event.type == pygame.QUIT:
             running = False
+        
+    # draw the game
+    screen.fill(color = (43, 61, 79))
+    pygame.display.flip()
 
 pygame.quit()
 
