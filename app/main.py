7b711ca80f36ecd73716dfb672c8ef2fe5ebb2a3
almightobasadre <alau.basadre.up@phinmaed.com>
2024-11-03 13:21:23 +0800

    Tried to make an object move in the screen

--- app/main.py
+++ app/main.py
@@ -8,6 +8,11 @@ pygame.display.set_caption("Creeps Shooter")
 
 running = True
 
+# Plain Surface
+surf = pygame.Surface((50, 50))
+surf.fill('blue')
+x = 100
+
 while running:
     for event in pygame.event.get(): # event loop
         if event.type == pygame.QUIT:
@@ -15,11 +20,15 @@ while running:
         
     # draw the game
     screen.fill(color = (43, 61, 79))
+    x += 0.1
+    screen.blit(surf, (x, 150))
+
     pygame.display.flip()
 
 pygame.quit()
 
 """
 Step 1: Initialize pygame.init() and screen properties, also always include pygame.quit()
-Step 2: Setup the game event loop and draw the game objects
+Step 2: Setup the game event loop
+Step 3: Draw the game objects using surface
 """
